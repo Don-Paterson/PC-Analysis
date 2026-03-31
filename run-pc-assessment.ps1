84fccea52a69610a8601d1a681455cf6e3006b6b
Don-Paterson <donpaterson@hotmail.com>
2026-03-31 15:03:06 +0100

    Update run-pc-assessment.ps1

--- run-pc-assessment.ps1
+++ run-pc-assessment.ps1
@@ -1,4 +1,5 @@
-$dest = "$env:USERPROFILE\Desktop\Assess-UserFiles.ps1"
+# $dest = "$env:USERPROFILE\Desktop\Assess-UserFiles.ps1"
+$dest = "$([Environment]::GetFolderPath('Desktop'))\Assess-UserFiles.ps1"
 Invoke-RestMethod "https://raw.githubusercontent.com/Don-Paterson/PC-Analysis/main/Assess-UserFiles.ps1" |
     Out-File -FilePath $dest -Encoding UTF8 -NoNewline
 pwsh -ExecutionPolicy Bypass -File $dest
